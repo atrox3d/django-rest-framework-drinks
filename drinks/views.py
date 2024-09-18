ebbc1041c304313e96728366f33d2f46132fd43d
atrox3d <atrox3d@gmail.com>
2024-09-18 08:45:05 +0200

    add drinks.views.drink_list

---
+++ drinks/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpRequest, JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+
+def drink_list(request:HttpRequest):
+    # get all drinks 
+    drinks = Drink.objects.all()
+    
+    # serialize them 
+    serializer = DrinkSerializer(drinks, many=True)
+
+    # return json
+    return JsonResponse(serializer.data)
+
