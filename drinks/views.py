3d41569e8bcb1bd7662d897c435963666290a1ec
atrox3d <atrox3d@gmail.com>
2024-09-19 07:56:43 +0200

    add drink

--- drinks/views.py
+++ drinks/views.py
@@ -1,19 +1,32 @@
 from django.http import HttpRequest, JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-
+@api_view(('GET', 'POST'))
 def drink_list(request:HttpRequest):
-    # get all drinks 
-    drinks = Drink.objects.all()
-    
-    # serialize them 
-    serializer = DrinkSerializer(drinks, many=True)
 
-    # return json
-    return JsonResponse(
-        {'drinks': serializer.data}, 
-        # safe=False
-    )
+    if request.method == 'GET':
+        # get all drinks 
+        drinks = Drink.objects.all()
+        
+        # serialize them 
+        serializer = DrinkSerializer(drinks, many=True)
 
+        # return json
+        return JsonResponse(
+            {'drinks': serializer.data}, 
+            # safe=False
+        )
+
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
 
